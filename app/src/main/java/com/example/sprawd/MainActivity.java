d5467554e7b376fc58d91ea47ea6be20d0c6d2c7
trzyczwarte <115505431+oliwierkoziol@users.noreply.github.com>
2025-12-03 13:11:50 +0100

    funkcjonalnosc start

--- app/src/main/java/com/example/sprawd/MainActivity.java
+++ app/src/main/java/com/example/sprawd/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +36,18 @@ public class MainActivity extends AppCompatActivity {
         CheckBox check2 = findViewById(R.id.check2);
         CheckBox check3 = findViewById(R.id.check3);
 
+        char[] lower = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char[] upper = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        char[] digits = {'1', '2','3','4','5','6','7','8','9','0'};
+        char[] symbols = {'!', '?',  '@', '#', '$', '%', '&'};
+
+        Random rnd = new Random();
+        String pass = "";
+
+        for (int i = 0; i < 8; i++) {
+            pass += lower[rnd.nextInt(lower.length)];
+        }
+
+        Toast.makeText(MainActivity.this, pass, Toast.LENGTH_LONG).show();
     }
 }

80ab15f8ee232a2ca02abf57f752da8d18e55fd3
trzyczwarte <115505431+oliwierkoziol@users.noreply.github.com>
2025-12-03 13:20:07 +0100

    funkcjonalnosc dalej

--- app/src/main/java/com/example/sprawd/MainActivity.java
+++ app/src/main/java/com/example/sprawd/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sprawd;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -12,6 +13,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,18 +40,31 @@ public class MainActivity extends AppCompatActivity {
         CheckBox check2 = findViewById(R.id.check2);
         CheckBox check3 = findViewById(R.id.check3);
 
-        char[] lower = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        char[] upper = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-        char[] digits = {'1', '2','3','4','5','6','7','8','9','0'};
-        char[] symbols = {'!', '?',  '@', '#', '$', '%', '&'};
+        generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Character[] lower = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+                Character[] upper = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+                Character[] digits = {'1', '2','3','4','5','6','7','8','9','0'};
+                Character[] symbols = {'!', '?',  '@', '#', '$', '%', '&'};
+                ArrayList<Character> all = new ArrayList<Character>();
 
-        Random rnd = new Random();
-        String pass = "";
+                if (check1.isChecked()) {
+                    all.addAll(Arrays.asList(lower));
+                    all.addAll(Arrays.asList(upper));
+                },
+                if (check2.isChecked()) all.addAll(Arrays.asList(digits));
+                if (check3.isChecked()) all.addAll(Arrays.asList(symbols));
 
-        for (int i = 0; i < 8; i++) {
-            pass += lower[rnd.nextInt(lower.length)];
-        }
+                Random rnd = new Random();
+                String pass = "";
 
-        Toast.makeText(MainActivity.this, pass, Toast.LENGTH_LONG).show();
+                for (int i = 0; i < 8; i++) {
+                    pass += all.get(rnd.nextInt(all.size()));
+                }
+
+                Toast.makeText(MainActivity.this, pass, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

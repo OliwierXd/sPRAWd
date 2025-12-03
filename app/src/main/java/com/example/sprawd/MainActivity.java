202a8e6f2c1f368a498f407e347860543dc8f4ff
trzyczwarte <115505431+oliwierkoziol@users.noreply.github.com>
2025-12-03 13:44:18 +0100

    wyswietlanie

--- app/src/main/java/com/example/sprawd/MainActivity.java
+++ app/src/main/java/com/example/sprawd/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.sprawd;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -40,27 +44,63 @@ public class MainActivity extends AppCompatActivity {
         CheckBox check2 = findViewById(R.id.check2);
         CheckBox check3 = findViewById(R.id.check3);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         generateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String lower = "abcdefghijklmnopqrstuvwxyz";
-                String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                String digits = "1234567890";
-                String symbols = "!@#$%^&*()_+-=.";
 
-
-                Random rnd = new Random();
-                String pass = "";
-
-                for (int i = 0; i < 8; i++) {
-                    if (i == 0 && check1.isChecked()) pass += upper.charAt(rnd.nextInt(upper.length()));
-                    else if (i == 1 && check2.isChecked()) pass += digits.charAt(rnd.nextInt(digits.length()));
-                    else if (i == 2 && check3.isChecked()) pass += symbols.charAt(rnd.nextInt(symbols.length()));
-                    else pass += lower.charAt(rnd.nextInt(lower.length()));
-                }
-
-                Toast.makeText(MainActivity.this, pass, Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Wygenerowane hasło");
+                builder.setMessage(generatePassword(check1.isChecked(), check2.isChecked(), check3.isChecked()))
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Xd!
+                            }
+                        });
+                builder.create().show();
             }
         });
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Wygenerowane hasło");
+                builder.setMessage("Imię: "+nameInput.getText().toString() + "\n" +
+                                "Nazwisko: "+surnameInput.getText().toString() + "\n" +
+                                "Stanowisko: "+surnameInput.getText().toString() + "\n" +
+                                "Hasło: "+generatePassword(check1.isChecked(), check2.isChecked(), check3.isChecked()) + "\n")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Xd!
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+    }
+
+    String generatePassword(boolean u, boolean d, boolean s) {
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String digits = "1234567890";
+        String symbols = "!@#$%^&*()_+-=.";
+
+
+        Random rnd = new Random();
+        String pass = "";
+
+        for (int i = 0; i < 8; i++) {
+            if (i == 0 && u) pass += upper.charAt(rnd.nextInt(upper.length()));
+            else if (i == 1 && d) pass += digits.charAt(rnd.nextInt(digits.length()));
+            else if (i == 2 && s) pass += symbols.charAt(rnd.nextInt(symbols.length()));
+            else pass += lower.charAt(rnd.nextInt(lower.length()));
+        }
+        return pass;
     }
 }
